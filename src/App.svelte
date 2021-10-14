1e8520420f127a2adfdfcb9a44a4accfc99ea3c4
Muiz Uvais <35157872+MuizU@users.noreply.github.com>
2021-10-14 22:35:37 +0530

    Added HTML for calculator (#3)
    * Changed title
    
    * added HTML for the calculator
    
    * added styling for calculator

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,127 @@
 <script>
   import Navbar from "./Navbar.svelte";
-  export let name;
 </script>
 
-
-<Navbar></Navbar>
+<Navbar />
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <div class="calculator-grid">
+    <div class="output">
+      <div class="previous-operand" />
+      <div class="current-operand" />
+    </div>
+    <button class="span-two">AC</button>
+    <button>DEL</button>
+    <button>÷</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>*</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>+</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>.</button>
+    <button>0</button>
+    <button class="span-two">=</button>
+  </div>
 </main>
 
 <style>
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    font-family: Gotham Rounded, sans-serif;
+    font-weight: normal;
+  }
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    margin: 0;
+    padding: 0;
+    background: linear-gradient(to right, #cbce91ff, #ea738dff);
+  }
+  .calculator-grid {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    grid-template-columns: repeat(4, 100px);
+    grid-template-rows: minmax(120px, auto) repeat(5, 100px);
+  }
+  .calculator-grid > button {
+    cursor: pointer;
+    font-size: 2rem;
+    border: 1px, solid #ffffff;
+    outline: none;
+    background-color: rbga(255, 255, 255, 0.75);
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .calculator-grid > button:hover {
+    background-color: #a9a9a9;
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    font-family: Gotham Rounded, sans-serif;
+    font-weight: normal;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  body {
+    margin: 0;
+    padding: 0;
+    background: linear-gradient(to right, #cbce91ff, #ea738dff);
+  }
+
+  .calculator-grid {
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    min-height: 100vh;
+    grid-template-columns: repeat(4, 100px);
+    grid-template-rows: minmax(120px, auto) repeat(5, 100px);
+  }
+
+  .calculator-grid > button {
+    cursor: pointer;
+    font-size: 2rem;
+    border: 1px, solid #ffffff;
+    outline: none;
+    background-color: rbga(255, 255, 255, 0.75);
+  }
+
+  .calculator-grid > button:hover {
+    background-color: #a9a9a9;
+  }
+
+  .span-two {
+    grid-column: span 2;
+    color: #adf802;
+    background-color: rgba(139, 0, 139, 0.8);
+  }
+
+  .output {
+    grid-column: 1 / -1;
+    background-color: rgba(0, 0, 0, 0.75);
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-around;
+    flex-direction: column;
+    padding: 10px;
+    word-wrap: break-word;
+    word-break: break-all;
+  }
+
+  .output .previous-operand {
+    color: rgba(255, 255, 255, 0.75);
+    font-size: 1.5rem;
+  }
+
+  .output .current-operand {
+    color: white;
+    font-size: 2.5rem;
   }
 </style>
-
